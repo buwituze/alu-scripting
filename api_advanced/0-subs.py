167c8157626d012d3935ee77e89108bfc8269cd4
buwituze <b.rutagengw@alustudent.com>
2024-03-03 15:02:26 +0200

    updating task0,1

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""0-subs.py"""
+"""
+0-subs
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Reddit subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
+    """Returns the number of subscribers for a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = \
+        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
+
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
     else:
-        return response.json().get("data").get("subscribers")
+        return 0
