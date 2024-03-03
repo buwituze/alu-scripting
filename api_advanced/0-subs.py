23dbc3ca9b2b4e43d0ab859fd2b9ee7052ecd996
buwituze <b.rutagengw@alustudent.com>
2024-03-03 15:11:26 +0200

    updaating tasks

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-0-subs
-"""
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
 
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
+    except Exception:
         return 0
